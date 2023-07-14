b124a73ddde07ae96befaf714807f5e60b224f4c
thuctran <tranduythuc2003@gmail.com>
2023-07-14 19:54:14 +0700

    fix tools and UI

--- UI_App.py
+++ UI_App.py
@@ -1,26 +1,48 @@
+import threading
 import tkinter as tk
+from tkinter import ttk
 import subprocess
 from tkinter import font
 import re
 import tools_regacc_opm as tool
+from tkinter import messagebox
 
+
+WITH_COMPONENT = 50
+HEIGHT_COMPONENT = 50 
 window = tk.Tk()
 window.geometry("500x300")
 window.title("Tools chơi game OPM server Trung")
 
 # component ----------------------------------------------------------------
-label = tk.Label(window, text = "Danh sách giả lập hiện tại", background="yellow",font= font.Font(weight= "bold", size= 12), borderwidth=1, relief= "solid")
-label.grid(column= 0, row= 0)
+label_list_devices = tk.Label(window, text = "Danh sách giả lập hiện tại", background="yellow",font= font.Font(weight= "bold", size= 12), borderwidth=1, relief= "solid")
+label_list_devices.grid(column= 0, row= 0)
+
+label_list_threads = tk.Label(window, text = "Danh sách luồng đang chạy", background="yellow",font= font.Font(weight= "bold", size= 12), borderwidth=1, relief= "solid")
+label_list_threads.grid(column= 3, row= 0, sticky="w")
+
 button = tk.Button(window, text="Reload")
 button.grid(column= 1, row= 0, padx= 10)
 
-listbox = tk.Listbox(window, selectmode=tk.MULTIPLE, width= 30)
-listbox.grid(column=0, row= 1, sticky= "w")
+listbox = tk.Listbox(window, selectmode=tk.MULTIPLE, width= 31)
+listbox.grid(column=0, row= 1, sticky= "nw")
+
+treeView_threads = ttk.Treeview(window, columns= ("name_thread", "status"), show= "headings", height= 7)
+treeView_threads.heading("name_thread", text="Tên luồng")
+treeView_threads.heading("status", text="Trạng thái")
+treeView_threads.column("name_thread", width=130)
+treeView_threads.column("status", width=90)
+treeView_threads.grid(column=3, row= 1, sticky= "w")
+
 btn_start = tk.Button(window,text = "Start", background="red",font= font.Font(weight= "bold", size= 10), borderwidth=1, relief= "solid", padx= 20)
 btn_start.grid(column= 0, row=2, pady= 10, sticky= "w")
 
 
+
 # function ----------------------------------------------------------------
+def show_message(mess):
+    messagebox.showinfo("Thong bao", mess)
+
 def getListEmulator():
     path = r"C:\Users\vytra\AppData\Local\Android\Sdk\platform-tools\adb"
     result = subprocess.run([path, "devices"], capture_output=True, text=True)
@@ -29,17 +51,42 @@ def getListEmulator():
     for i in re.findall(pattern, result.stdout):
         listbox.insert(tk.END,i)
 
-def getEmulatorFromList():
+def getEmulatorFromList(listbox_):
     selections_index = listbox.curselection()
-    return [listbox.get(index) for index in selections_index]
+    return [listbox_.get(index) for index in selections_index]
 
-def startTools():
-    list_devices = getEmulatorFromList()
+def startTools(name):
+    try:
+        window.after(0, show_message, "Thanh Cong")
+        tool.start_tool(name)
+    except:
+        pass
+
+def create_thread():
+    list_devices = getEmulatorFromList(listbox)
     for name_device in list_devices:
-        tool.start_tool(name_device)
+        thread = threading.Thread(target= startTools, args=(name_device,))
+        thread.start()
+        thread.name = "Thread: " + name_device
+        status = "Alive" if thread.is_alive() else "Dead"
+        treeView_threads.insert("", "end", values= (thread.name, status))
+
+def find_thread(name):
+    for thread in threading.enumerate():
+        if thread.name == name:
+            return thread
+
+def update_status_of_thread():
+    for thread in threading.enumerate():
+        status = "Alive" if thread.is_alive() else "Dead"
+        index = treeView_threads.index(thread.name)
+        treeView_threads.item(treeView_threads.get_children("")[index], values=(thread.name, status))
+
 
 # event --------------------------------
+
 button.bind("<Button-1>", lambda a: getListEmulator())
-btn_start.bind("<Button-1>", lambda a: startTools())
+btn_start.bind("<Button-1>", lambda a: create_thread())
+window.after(10, update_status_of_thread)
 window.mainloop()
 
--- tools_regacc_opm.py
+++ tools_regacc_opm.py
@@ -1,74 +1,123 @@
+import os
+import cv2
+import numpy as np
 import uiautomator2 as u2
 import time
 import random
 import string
 
+
+
 def create_username():
-    return  'a' + ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+    return  'a' + ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(11))
 
-
-
+def check_image(image_check_path, image_source_path):
+    image_check = cv2.imread(image_check_path,0)
+    image_source = cv2.imread(image_source_path,0)
+    
+    result = cv2.matchTemplate(image_source, image_check, cv2.TM_CCOEFF_NORMED)
+    
+    threshold = 0.8
+    
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+    if locations:
+        center_x = np.mean([x for x, y in locations])
+        center_y = np.mean([y for x, y in locations])
+        os.remove(image_source_path)
+        return (center_x, center_y)
+    else:
+        return None
 # ----------------------------------------------------------------
-# start tools
+# start tool
 
 def start_tool(emulator):
-    d = u2.connect(emulator)
     
+    d = u2.connect(emulator)
+    screen_size = d.window_size()
     element = d(text="一拳超人：最强之男")
     if element.exists():
         element.click()
-        d.implicitly_wait(70.0)
 
-    # Dang ki acc
-    exit_signUp = d(resourceId = "com.flamingo.sdk:id/view_login_welcome_switch_account")
-    if(exit_signUp.exists()):
-        exit_signUp.click()
-
-    sign_up = d(text="快速注册")
-    sign_up.click()
-
+    switch_acc = d(resourceId = "com.flamingo.sdk:id/view_login_welcome_switch_account")
     user_name = d(resourceId ="com.flamingo.sdk:id/view_register_user_name")
-    user_name.clear_text()
-    text = create_username()
-    with open("data.txt", "w") as f:
-        f.write(text)
-    user_name.set_text(text)
-
     password = d(resourceId ="com.flamingo.sdk:id/view_register_password")
-    password.clear_text()
-    password.set_text("Thuc12345")
     checkbox = d(resourceId ="com.flamingo.sdk:id/view_protocol_checkbox")
-    checkbox.click()
     verify = d(resourceId ="com.flamingo.sdk:id/view_register_commit")
-    verify.click()
-
-    # xac minh cmnd
-
+    sign_up = d(text= "快速注册")
+    user_name = d(resourceId ="com.flamingo.sdk:id/view_register_user_name")
     identity_name = d(resourceId ="com.flamingo.sdk:id/view_dialog_verify_identity_name")
-    identity_name.set_text("蔡培初")
-
     identity_num = d(resourceId ="com.flamingo.sdk:id/view_dialog_verify_identity_num")
-    identity_num.set_text("360311198512161574")
-
     identity_commit = d(resourceId ="com.flamingo.sdk:id/view_dialog_verify_identity_commit")
-    identity_commit.click()
-
     btnOk = d(resourceId ="com.flamingo.sdk:id/gp_sdk_save_pic_btn_know")
-    btnOk.click()
-
-    # xoa banner va vao game
     today_no_more_notice = d(resourceId ="com.flamingo.sdk:id/today_no_more_notice")
-    today_no_more_notice.click_exists()
-
     push_close = d(resourceId ="com.flamingo.sdk:id/push_close")
-    push_close.click_exists()
+    
+    time.sleep(5)
+    while True:
+        start_time = time.time()
+        if(switch_acc.exists()):
+            try:
+                switch_acc.click()
+                break
+            except:
+                time.sleep(3)
+                pass
+        
+    while True:
+        if(sign_up.exists()):
+            try:
+                sign_up.click()
+                user_name.clear_text()
+                text = create_username()
+                with open("Tools_game_OPM/data.txt", "a") as f:
+                    f.write(text + '\n')
+                user_name.set_text(text)
+                password.clear_text()
+                password.set_text("Thuc12345")
+                checkbox.click()
+                verify.click()
+                break
+            except:
+                time.sleep(1)
+                pass
 
-    screen_size = d.window_size()
+    while True:
+        start_time = time.time()
+        if(identity_name.exists()):
+            try:
+                identity_name.set_text("蔡培初")
+                identity_num.set_text("360311198512161574")
+                identity_commit.click()
+                btnOk.click()
+                break
+            except:
+                time.sleep(1)
+                pass
 
-    x = screen_size[1]*0.9
-    y= screen_size[0]*0.1
-    time.sleep(35)
-    d.click(x,y)
+    while True:
+        time_start = time.time()
+        if(push_close.exists()):
+            try:
+                today_no_more_notice.click_exists()
+                push_close.click_exists()
+                break
+            except:
+                time.sleep(1)
+                pass
+    
+    while True:
+        try:
+            pic = d.screenshot()
+            pic.save("Tools_game_OPM/image/abd.png")
+            location = check_image("Tools_game_OPM/image/exit.png","Tools_game_OPM/image/abd.png")
+            x = location[0]
+            y= location[1]
+            d.click(x,y)
+            break
+        except:
+            time.sleep(1)
+            pass
 
     x = screen_size[1]*0.5
     y = screen_size[0]*0.69
